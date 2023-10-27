40409b61dd79af7dc6037792b5205b84e1fbb3a1
DESKTOP-5GJCEAC\Administrator <dkwnstkdk@gmail.com>
2023-10-27 14:55:40 +0900

    Create Lotto vo and service

---
+++ src/main/java/com/lotto/web/model/vo/LottoVO.java
@@ -0,0 +1,15 @@
+package com.lotto.web.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LottoVO {
+    private int firstNumber;
+    private int secondeNumber;
+    private int thirdNumber;
+    private int fourthNumber;
+    private int fifthNumber;
+    private int sixthNumber;
+}
---
+++ src/main/java/com/lotto/web/service/LottoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.lotto.web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LottoServiceImpl implements LottoService{
+
+}
