ed475b2a047b8f7b8e80c17f3aafe1126da02754
junsang-ee <dkwnstkddk@gmail.com>
2023-12-11 15:29:22 +0900

    Define ErrorMessage class

--- src/main/java/com/lotto/web/constants/messages/ErrorMessage.java
+++ src/main/java/com/lotto/web/constants/messages/ErrorMessage.java
@@ -2,7 +2,6 @@ package com.lotto.web.constants.messages;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.experimental.Accessors;
 
 @Getter
 @AllArgsConstructor
@@ -10,8 +9,26 @@ public enum ErrorMessage {
     NEEDS_NUMBER_OVER("100"),
     EXCEPTION_NUMBER_OVER("101"),
     PRICE_UNIT_INCONSISTENT("103"),
-    USER_NOT_FOUND("500");
+    USER_NOT_FOUND("500"),
+    UNKNOWN("9999");
 
-    @Accessors(fluent = true)
+
     private final String code;
+
+    public String resName() {
+        return "response.error." + name().toLowerCase().replaceAll("_", ".") + ".message";
+    }
+
+    public int getCode() {
+        return Integer.parseInt(code);
+    }
+
+    public static ErrorMessage from(String code) {
+        for (ErrorMessage value : values()) {
+            if (value.code.equals(code)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException(code);
+    }
 }
