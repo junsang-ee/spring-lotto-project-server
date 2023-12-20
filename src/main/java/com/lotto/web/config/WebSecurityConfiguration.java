f52e6961a376c86dfcff306af0ad0f1b6afd0989
junsang-ee <dkwnstkddk@gmail.com>
2023-12-20 17:38:26 +0900

    Remove unnecessary dependancy module and Add encryptPasswordEncoder Bean In WebSecurityConfiguration Add passwordEncoder In UserService

--- src/main/java/com/lotto/web/config/WebSecurityConfiguration.java
+++ src/main/java/com/lotto/web/config/WebSecurityConfiguration.java
@@ -9,10 +9,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.AntPathMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +32,10 @@ public class WebSecurityConfiguration {
         "/api/admin/**"
     };
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/lotto/web/service/UserServiceImpl.java
+++ src/main/java/com/lotto/web/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.lotto.web.service;
 import com.lotto.web.model.entity.UserEntity;
 import com.lotto.web.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public UserEntity get(String userId) {
         return null;
