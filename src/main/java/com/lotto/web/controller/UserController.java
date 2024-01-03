a2e997baed711e34492d23f02d997feb12d78d58
junsang-ee <dkwnstkddk@gmail.com>
2024-01-03 15:48:20 +0900

    Removed AbstractBoardBaseEntity (create CreationUserEntity) Add API getUserDetail In UserController

---
+++ src/main/java/com/lotto/web/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.lotto.web.controller;
+
+import com.lotto.web.model.dto.response.UserDetailResponse;
+import com.lotto.web.model.dto.response.common.ApiSuccessResponse;
+import com.lotto.web.model.entity.UserEntity;
+import com.lotto.web.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+@RestController
+public class UserController extends BaseController {
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public ApiSuccessResponse<UserDetailResponse> me(@AuthenticationPrincipal(expression = "id") String userId) {
+        return wrap(userService.getDetail(userId));
+    }
+}
