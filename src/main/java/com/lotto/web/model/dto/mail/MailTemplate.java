cb54a3b8dfcbd89761d780f684d8a33a7c460a08
DESKTOP-5GJCEAC\Administrator <dkwnstkdk@gmail.com>
2024-01-15 11:37:05 +0900

    Modify MailTemplate

--- src/main/java/com/lotto/web/model/dto/mail/MailTemplate.java
+++ src/main/java/com/lotto/web/model/dto/mail/MailTemplate.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MailTemplate {
-    VERIFY_EMAIL("JUNSANG_LOTTO 이메일 주소 인증"),
-    RESET_PASSWORD("JUNSANG_LOTTO 비밀번호 변경 안내");
+    VERIFY_EMAIL("JUNSANG LOTTO 이메일 주소 인증"),
+    RESET_PASSWORD("JUNSANG LOTTO 비밀번호 변경 안내");
 
     private final String subject;
 
