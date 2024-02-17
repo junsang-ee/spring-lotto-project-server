2fb96c8cb2cf89e864df7f3dc65885cf839bedeb
junsang-ee <dkwnstkddk@gmail.com>
2024-02-17 19:26:24 +0900

    Create and Add ReplyCountEntity

---
+++ src/main/java/com/lotto/web/model/entity/count/ReplyCountEntity.java
@@ -0,0 +1,30 @@
+package com.lotto.web.model.entity.count;
+
+import com.lotto.web.constants.UserStatus;
+import com.lotto.web.model.SequentialEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "reply_count")
+public class ReplyCountEntity extends SequentialEntity {
+
+    private int enabledCount;
+
+    private int disabledCount;
+
+
+    @PrePersist
+    private void onPrevisionPersist() {
+        this.enabledCount = 0;
+        this.disabledCount = 0;
+    }
+}
