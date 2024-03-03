03cc7f91d223621f13e7f20f74b6c74827dc2275
junsang-ee <dkwnstkddk@gmail.com>
2024-03-03 12:47:18 +0900

    Create and Add PostCountEntity

---
+++ src/main/java/com/lotto/web/model/entity/count/PostCountEntity.java
@@ -0,0 +1,28 @@
+package com.lotto.web.model.entity.count;
+
+import com.lotto.web.model.SequentialEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "post_count")
+@Table(name = "post_count")
+public class PostCountEntity extends SequentialEntity {
+
+    private int enabledCount;
+
+    private int disabledCount;
+
+    @PrePersist
+    private void onPrevisionPersist() {
+        this.enabledCount = 0;
+        this.disabledCount = 0;
+    }
+}
