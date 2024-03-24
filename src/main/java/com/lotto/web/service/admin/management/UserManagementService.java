3132f84c87e5586cec4bb2b78e7b21ed589b2384
junsang-ee <dkwnstkddk@gmail.com>
2024-03-24 18:25:06 +0900

    Remove unnecessary symbol

--- src/main/java/com/lotto/web/service/admin/management/UserManagementService.java
+++ src/main/java/com/lotto/web/service/admin/management/UserManagementService.java
@@ -1,10 +1,8 @@
 package com.lotto.web.service.admin.management;
 
 import com.lotto.web.constants.UserStatus;
-import com.lotto.web.model.dto.response.admin.UserExtractionListResponse;
 import com.lotto.web.model.dto.response.admin.UserManageDetailResponse;
 import com.lotto.web.model.dto.response.admin.UserManageListResponse;
-import com.lotto.web.model.dto.response.admin.UserPostListResponse;
 import com.lotto.web.model.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
